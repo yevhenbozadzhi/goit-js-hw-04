201eceedc0e1adbcf3345fe185831bd03c42bb03
Yevhen <ebozadji13@gmail.com>
2025-03-30 14:32:38 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+function filterArray(numbers, value) {
+    const stx = [];
+    for (const number of numbers) {
+        if (number > value) {
+            stx.push(number);
+    }
+    }
+    return stx;
+}
+
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
